c5608de3a665199e6d61a95ad593e4b358b4f7ef
y.shinkorenko <y.shinkorenko@g.nsu.ru>
2024-09-26 18:24:36 +0300

    complite lab0

---
+++ lab0/WordCounter.h
@@ -0,0 +1,20 @@
+#ifndef WORDCOUNTER_H
+#define WORDCOUNTER_H
+
+#include <map>
+#include <list>
+#include <vector>
+
+class WordCounter {
+private:
+    std::map<std::string, int> wordCount;
+    int totalWords{0};
+    bool isDelimiter(char c);
+    std::list<std::string> splitIntoWords(const std::string& line);
+public:
+    void countWordsFromFile(const std::string& filename);
+    std::vector<std::pair<std::string, int>> getSortedWord() const;
+    int getTotalWords() const;
+};
+
+#endif
